2609c3f5219e7747e93f56678bf168c92d89ecd4
Keita Kohga <kogaspain@gmail.com>
2020-04-18 11:09:58 +0900

    create_nuxt_project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<v-layout column justify-center align-center>
+  <v-flex xs12 sm8 md6>
+    <div class="text-xs-center">
+      <h1>Hello Nuxt</h1>
+    </div>
+  </v-flex>
+</v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
